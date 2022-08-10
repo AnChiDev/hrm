f807b8fcf7198a6d6cc43e3704d2a6ffe0bff180
MinhTran <designbyanchi@gmail.com>
2022-08-11 00:01:57 +0700

    update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,11 @@ return(
   <div>
   <Navbar bg="primary" expand="lg" >
   <Container>
-    <Navbar.Brand to="/home" className ="text-light fs-3"><i class="fa fa-vine" aria-hidden="true"></i></Navbar.Brand>
+    <Navbar.Brand href="/" className ="text-light fs-3"><i class="fa fa-vine" aria-hidden="true"></i></Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link to="/menu"  className ="nav-link text-light fs-5"><i class="fa fa-users" aria-hidden="true"> Nhân viên</i></Nav.Link>
+        <Nav.Link href="/"  className ="text-light fs-5"><i class="fa fa-users" aria-hidden="true"> Nhân viên</i></Nav.Link>
         <Nav.Link href="/"  className ="text-light fs-5"><i class="fa fa-id-card-o" aria-hidden="true"> Phòng ban</i></Nav.Link>
         <Nav.Link href="/"  className ="text-light fs-5"><i class="fa fa-money" aria-hidden="true"> Bảng lương</i></Nav.Link>
       </Nav>
